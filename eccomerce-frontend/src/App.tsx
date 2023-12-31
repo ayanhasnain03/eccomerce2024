ce6e50ea3e78f9465a56d394610e2ff9404dba8d
Ayan <ayan2572006@gmail.com>
2023-12-31 16:20:22 +0530

    shipping form

--- eccomerce-frontend/src/App.tsx
+++ eccomerce-frontend/src/App.tsx
@@ -2,9 +2,11 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { lazy,Suspense } from "react";
 import Loader from "./components/loader";
 import Header from "./components/header";
+
 const  Home =lazy(()=>import("./pages/home"))
 const Search = lazy(()=>import("./pages/search"))
 const Cart = lazy(()=>import("./pages/cart"))
+const Shipping = lazy(()=>import("./pages/shipping"))
 //Admin Routes Importing
 // Admin Routes Importing
 const Dashboard = lazy(() => import("./pages/admin/dashboard"));
@@ -29,12 +31,20 @@ const TransactionManagement = lazy(
 const App = () => {
   return (
     <Router>
+      {/* Header */}
       <Header/>
     <Suspense fallback={<Loader/>}>
     <Routes>
         <Route path="/" element={<Home />}/>
         <Route path="/search" element={<Search/>} />
         <Route path="/cart" element={<Cart />}/>
+
+
+
+        {/* Logged in User Routes */}
+        <Route>
+        <Route path="/shipping" element={<Shipping />}/>
+        </Route>
 
 
 
