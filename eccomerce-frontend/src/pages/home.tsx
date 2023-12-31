043613ff184b92625674b4bab1ceef24b2e426ab
Ayan <ayan2572006@gmail.com>
2023-12-31 14:07:59 +0530

    Home

--- eccomerce-frontend/src/pages/home.tsx
+++ eccomerce-frontend/src/pages/home.tsx
@@ -1,6 +1,21 @@
+import { Link } from "react-router-dom"
+import ProductCard from "../components/product-card"
+
 const Home = () => {
+  const addToCartHandler=()=>{}
   return (
-    <div>Home</div>
+   <div className="home">
+    <section></section>
+
+    <h1>Latest Products
+    <Link to="/search" className="findmore">
+      More
+    </Link>
+    </h1>
+    <main>
+      <ProductCard productId="asasas" name="Vivobook" price={99999} stock={20} handler={addToCartHandler} photo="https://rukminim2.flixcart.com/image/312/312/xif0q/computer/c/l/t/-original-imagtucmkuwggepy.jpeg?q=70"/>
+    </main>
+   </div>
   )
 }
 
