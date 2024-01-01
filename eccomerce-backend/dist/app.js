ab478a7ae015eb0eb3dd4ccff154d3e63f6760cb
Ayan <ayan2572006@gmail.com>
2024-01-01 09:01:13 +0530

    user create

---
+++ eccomerce-backend/dist/app.js
@@ -0,0 +1,15 @@
+import express from "express";
+import userRoutes from "./routes/user.js";
+import { connectDb } from "./utils/features.js";
+const port = 4000;
+connectDb();
+const app = express();
+app.use(express.json());
+// importing Routes
+app.use("/api/v1/user", userRoutes);
+app.get("/", (req, res, next) => {
+    res.send("hello");
+});
+app.listen(port, () => {
+    console.log(`server is working on http://localhost:${port}`);
+});
---
+++ eccomerce-backend/src/utils/features.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose"
+export const connectDb = ()=>{
+mongoose.connect("mongodb://localhost:27017",{
+    dbName:"Eccomercem24",
+}).then(c=>{
+    console.log(`DB Connect to ${c.connection.host}`)
+}).catch(e=>{
+    console.log(console.log(e))
+})
+}
