7ab4039cc89d248a1775e7c87aef6f61cf70b426
Ayan <ayan2572006@gmail.com>
2024-01-06 02:30:09 +0530

    fix error

--- eccomerce-frontend/src/redux/reducer/userReducer.ts
+++ eccomerce-frontend/src/redux/reducer/userReducer.ts
@@ -1,6 +1,6 @@
 import { PayloadAction, createSlice } from "@reduxjs/toolkit"; 
 import { UserReducerInitialState } from "../../types/reducer-types";
-import { User } from "firebase/auth";
+import { User } from "../../types/types";
 
 const initialState:UserReducerInitialState={
     user:null,
@@ -10,7 +10,7 @@ export const userReducer = createSlice({
     name:"userReducer",
     initialState,
     reducers:{
-userExist:(state,action:PayloadAction<User>)=>{
+userExist:(state,action:PayloadAction <User>)=>{
     state.loading=false;
     state.user=action.payload;
 },
